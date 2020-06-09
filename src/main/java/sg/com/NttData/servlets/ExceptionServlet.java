d18ff91851924c7f0da65ea6376691b0c1b40227
Johji Yamada <johji.yamada@openweb.corp>
2020-06-09 22:22:59 +0800

    add exceptionServlet

--- src/main/java/sg/com/NttData/servlets/ExceptionServlet.java
+++ src/main/java/sg/com/NttData/servlets/ExceptionServlet.java
@@ -7,7 +7,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("/input")
+@WebServlet("/error")
 public class ExceptionServlet extends CommonServlet {
 
 	/**
