51341a1b485cf36cc6b74534b900a8127b8e9419
Johji Yamada <johji.yamada@openweb.corp>
2020-06-19 16:25:14 +0800

    version instead of deploy date

--- src/main/java/sg/com/NttData/servlets/TopServlet.java
+++ src/main/java/sg/com/NttData/servlets/TopServlet.java
@@ -9,28 +9,8 @@ import javax.servlet.http.HttpServletResponse;
 
 @WebServlet("/top")
 public class TopServlet extends CommonServlet {
-	public static String version = System.getenv("HEROKU_RELEASE_CREATED_AT");
-	
-	static {
-		try {
-			String utcStr = System.getenv("HEROKU_RELEASE_CREATED_AT");
-			version = utcStr;
-//			SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'");
-//	
-//			sdf.setTimeZone(TimeZone.getTimeZone("UTC"));
-//			Date deoloyDate = sdf.parse(utcStr);
-//			String version = deoloyDate.toString();
-//			System.out.println(version);
-//			SimpleDateFormat sdf2 = new SimpleDateFormat("yyyy-MM-dd' 'HH:mm:ss");
-//			sdf2.setTimeZone(TimeZone.getTimeZone("Asia/Singapore"));
-//			String version2 = sdf2.format(deoloyDate);
-//			System.out.println(version2);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
-
-	
+	public static String version = System.getenv("HEROKU_RELEASE_VERSION");
+		
 	/**
 	 * 
 	 */
