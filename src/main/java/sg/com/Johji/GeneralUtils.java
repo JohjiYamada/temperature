338bfc541c3d4d8eac8fde1080540e1fc533caf7
Johji Yamada <johji.yamada@openweb.corp>
2020-08-25 15:38:00 +0800

    Annie

--- src/main/java/sg/com/Johji/GeneralUtils.java
+++ src/main/java/sg/com/Johji/GeneralUtils.java
@@ -18,16 +18,16 @@ public class GeneralUtils {
 	
 //	only 7:00 am - 9:05 am, 12pm - 15:05pm
 	public static boolean isOpen() {
-		if (!isHeroku()) {
+//		if (!isHeroku()) {
 			return true;
-		}
-		Calendar cal = Calendar.getInstance();
-		// Convert Calendar to LocalTime
-		Instant instant = Instant.ofEpochMilli(cal.getTimeInMillis());
-		LocalTime now = LocalDateTime.ofInstant(instant, ZoneId.of("Asia/Singapore")).toLocalTime();
-		
-		return (now.isAfter(LocalTime.of(7, 0)) && now.isBefore(LocalTime.of(9, 5))) ||
-				(now.isAfter(LocalTime.of(12, 0)) && now.isBefore(LocalTime.of(15, 5)));
+//		}
+//		Calendar cal = Calendar.getInstance();
+//		// Convert Calendar to LocalTime
+//		Instant instant = Instant.ofEpochMilli(cal.getTimeInMillis());
+//		LocalTime now = LocalDateTime.ofInstant(instant, ZoneId.of("Asia/Singapore")).toLocalTime();
+//		
+//		return (now.isAfter(LocalTime.of(7, 0)) && now.isBefore(LocalTime.of(9, 5))) ||
+//				(now.isAfter(LocalTime.of(12, 0)) && now.isBefore(LocalTime.of(15, 5)));
 		
 	}
 }
