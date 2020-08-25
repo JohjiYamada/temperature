2a444934a8f5e960fed1abe6af743a25d3971f4a
Johji Yamada <johji.yamada@openweb.corp>
2020-08-25 18:14:43 +0800

    Annie

--- src/main/java/sg/com/Johji/SpreadSheetAccesser.java
+++ src/main/java/sg/com/Johji/SpreadSheetAccesser.java
@@ -3,22 +3,17 @@ package sg.com.Johji;
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.security.GeneralSecurityException;
 import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 
 import com.google.api.client.auth.oauth2.Credential;
-import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInstalledApp;
-import com.google.api.client.extensions.jetty.auth.oauth2.LocalServerReceiver;
-import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
-import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
+import com.google.api.client.googleapis.auth.oauth2.GoogleCredential;
 import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
 import com.google.api.client.http.javanet.NetHttpTransport;
 import com.google.api.client.json.JsonFactory;
 import com.google.api.client.json.jackson2.JacksonFactory;
-import com.google.api.client.util.store.FileDataStoreFactory;
 import com.google.api.services.sheets.v4.Sheets;
 import com.google.api.services.sheets.v4.SheetsScopes;
 import com.google.api.services.sheets.v4.model.ValueRange;
@@ -56,17 +51,22 @@ public class SpreadSheetAccesser {
 //		GoogleClientSecrets clientSecrets = JSON_FACTORY.fromString(CRE_STR, GoogleClientSecrets.class);
 
 		InputStream is = new ByteArrayInputStream(CRE_STR.getBytes());
-		GoogleClientSecrets clientSecrets = GoogleClientSecrets.load(JSON_FACTORY, new InputStreamReader(is));
+		GoogleCredential credential = JSON_FACTORY.fromString(CRE_STR, GoogleCredential.class);
+		return credential;
+//		GoogleClientSecrets clientSecrets = GoogleClientSecrets.load(JSON_FACTORY, new InputStreamReader(is));
 
+		
+//		InputStream is = GoogleSheetsCredential.class.getResourceAsStream("/sheets-service-account.json");
+//		credential = GoogleCredential.fromStream(is).createScoped(Collections.singleton(SheetsScopes.SPREADSHEETS));
 		
 		
 		// Build flow and trigger user authorization request.
-		GoogleAuthorizationCodeFlow flow = new GoogleAuthorizationCodeFlow.Builder(HTTP_TRANSPORT, JSON_FACTORY,
-				clientSecrets, SCOPES)
-						.setDataStoreFactory(new FileDataStoreFactory(new java.io.File(TOKENS_DIRECTORY_PATH)))
-						.setAccessType("offline").build();
-		LocalServerReceiver receiver = new LocalServerReceiver.Builder().setPort(8888).build();
-		return new AuthorizationCodeInstalledApp(flow, receiver).authorize("user");
+//		GoogleAuthorizationCodeFlow flow = new GoogleAuthorizationCodeFlow.Builder(HTTP_TRANSPORT, JSON_FACTORY,
+//				clientSecrets, SCOPES)
+//						.setDataStoreFactory(new FileDataStoreFactory(new java.io.File(TOKENS_DIRECTORY_PATH)))
+//						.setAccessType("offline").build();
+//		LocalServerReceiver receiver = new LocalServerReceiver.Builder().setPort(8888).build();
+//		return new AuthorizationCodeInstalledApp(flow, receiver).authorize("user");
 	}
 
 	/**
